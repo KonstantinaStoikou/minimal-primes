9d5371c64db3afaf12f4e20d0937c0e2106be4d6
Konna <tinasto97@gmail.com>
2019-09-28 20:03:34 +0300

    Refactor code and remake readme

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 ## Program that finds minimal prime numbers
+According to Wikipedia's [Minimal prime (recreational mathematics)](https://en.wikipedia.org/wiki/Minimal_prime_(recreational_mathematics))
+> In recreational number theory, a minimal prime is a prime number for which there is no shorter subsequence of its digits in a given base that form a prime.
+
 Define the number (MAXNUMB) until the program will search for minimal primes (default 100000000).  
 The program might run for some seconds until all minimal primes are found.  
+Compile and run:
+```
+$ gcc -o minprimes minprimes.c -lm
+$ ./minprimes
+```
 Output:
 ```
+2
+3
+5
+7
 11
 19
 41
--- minprimes.c
+++ minprimes.c
@@ -1,73 +1,87 @@
-/*File: minprimes.c*/
+#include <math.h>
 #include <stdio.h>
 #define MAXNUMB 100000000
 
-int main(void){
-	int i,x=11,flag1,b,num,k,y,mask,digits,max,flag3,a,flag2,flag4,flag5,flag6;
-	while(x <= MAXNUMB){
-		flag4 = 1;
-		flag5 = 1;
-		flag6 = 1;
-		i=3;
-		if(x%10 == 1 || x%10 == 9) flag1 = 1; //if x ends in 2,3,5 or 7 then one subsequence will be prime.If it ends in 0,4,6 or 8 then x is divided by 2 which means x is not a prime
-		else flag1 = 0;
-		while(i*i<= x && flag1 == 1){
-			if (x%i == 0) flag1 = 0;
-			i+=2;
-		}
-		if (flag1 == 1){
-		    flag3=1;
-			num=x;
-			digits=0;
-			max=1;
-			while ( num != 0 ) {
-				num /= 10;
-				if (num%10 == 2 || num%10 == 3 || num%10 == 5 || num%10 == 7) flag6 = 0;
-				digits++;
-			}
-			if (digits == 7){
-					if (x/1000000 == 2 || x/1000000 == 3 || x/1000000 == 5 || x/1000000 == 7) flag4 = 0;
-			}
-			else if (digits == 8){
-					if (x/10000000 == 2 || x/10000000 == 3 || x/10000000 == 5 || x/10000000 == 7) flag5 = 0;
-
-			}
-			for ( b = 1; b <= digits; b++ ) {
-				max *= 2;
-			}
-			b=1;
-			while(b < max-1 && flag3 == 1 && flag6 == 1){
-				flag2=1;
-				mask = b;
-				k = 1;
-				num = x;
-				y=0;
-				while ( num != 0) {
-					if ( mask % 2 == 1 ) {
-						y += num % 10 * k;
-						k *= 10;
-					}
-					num /= 10;
-					mask /= 2;
-
-				}
-				b++;
-				a=2;
-				while((a*a <= y || y == 1 || y == 0) && flag2 == 1){
-					if(y%a == 0 || y == 1 || y == 0) flag2 = 0;
-					a++;
-				}
-		    	if(flag2 == 0)
-					flag3 = 1;
-				else
-					flag3 = 0;
-			}
-			if (flag6 == 0) flag3 = 0;
-			if (flag3 == 1) printf("%d\n",x);
- 		}
- 		if (flag4 == 0) x+=1000000;       //for example if program reaches number 7000000 it continues with number 8000000 because subsequence 7 will be a prime
- 		else if (flag5 == 0) x+=10000000;
-		else x+=2;
+int is_prime(int num) {
+    if (num == 0 || num == 1) {
+        return 0;
     }
-	return 0;
+    int prime_flag = 1;
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            prime_flag = 0;
+            break;
+        }
+    }
+    return prime_flag;
+}
+
+int main(void) {
+    int x = 1;
+    while (x <= MAXNUMB) {
+        if (x / 10 == 0) {
+            if (is_prime(x)) {
+                printf("%d\n", x);
+            }
+            x++;
+            continue;
+        }
+        int last_digit = x % 10;
+        // numbers ending in 0,2,4,5,6,8 are devided either by 2 or 5 so
+        // they are not primes and numbers that contain 2,3,5,7 have a
+        // subsequence that is a prime, so they are not minimal primes
+        if (last_digit != 1 && last_digit != 9) {
+            x++;
+            continue;
+        }
+        // numbers containing 2,3,5,7 are not minimal primes
+        int copy_x = x;
+        int count_digits = 0;
+        while (copy_x != 0) {
+            int digit = copy_x % 10;
+            if (digit == 2 || digit == 3 || digit == 5 || digit == 7) {
+                break;
+            }
+            copy_x /= 10;
+            count_digits++;
+        }
+        // if x contains one of the digits not allowed then skip all
+        // "thousands"
+        if (copy_x != 0) {
+            x += pow(10, count_digits);
+            continue;
+        }
+        if (!is_prime(x)) {
+            x++;
+            continue;
+        }
+
+        // find all subsequences of the digits of x
+        int subseq_flag = 1;
+        int max = pow(2, count_digits);
+        for (int i = 1; i < max - 1; i++) {
+            int mask = i;
+            int k = 1;
+            int num = x;
+            int subseq = 0;
+            while (num != 0) {
+                if (mask % 2 == 1) {
+                    subseq += num % 10 * k;
+                    k *= 10;
+                }
+                num /= 10;
+                mask /= 2;
+            }
+            if (is_prime(subseq)) {
+                subseq_flag = 0;
+                break;
+            }
+        }
+        if (subseq_flag == 1) {
+            printf("%d\n", x);
+        }
+        x++;
+    }
+
+    return 0;
 }
